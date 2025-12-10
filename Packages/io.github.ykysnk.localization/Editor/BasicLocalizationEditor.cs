349e7b16bc261fc5dd50b9f5320c47e6a7f925d5
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-10 21:08:42 +0800

    fix: Update BasicTranslateDrawer and BasicLocalizationEditor to use consistent localization labels

--- Packages/io.github.ykysnk.localization/Editor/BasicLocalizationEditor.cs
+++ Packages/io.github.ykysnk.localization/Editor/BasicLocalizationEditor.cs
@@ -10,61 +10,15 @@ namespace io.github.ykysnk.Localization.Editor
     [CanEditMultipleObjects]
     public class BasicLocalizationEditor : BasicEditor
     {
-        private const string LocalizationIDProp = "localizationID";
-        private const string DisplayNameProp = "displayName";
-        private const string TranslatesProp = "translates";
         [SerializeField] private StyleSheet? uss;
         [SerializeField] private VisualTreeAsset? uxml;
-
-        private SerializedProperty? _displayName;
-        private SerializedProperty? _localizationID;
-        private SerializedProperty? _translates;
-
-        protected override void OnEnable()
-        {
-            _localizationID = serializedObject.FindProperty(LocalizationIDProp);
-            _displayName = serializedObject.FindProperty(DisplayNameProp);
-            _translates = serializedObject.FindProperty(TranslatesProp);
-        }
 
         protected override VisualElement? CreateErrorHandleInspectorGUI()
         {
             var root = new VisualElement();
             var visualTree = uxml!.CloneTree();
+            GlobalLocalization.DefaultHelper.UILocalize(visualTree);
             root.Bind(serializedObject);
-
-            var localizationIDField = visualTree.Q<TextField>("localizationID");
-
-            localizationIDField.label = _localizationID?.S();
-            localizationIDField.tooltip = _localizationID?.Tooltip();
-
-            _localizationID?.Register((label, tooltip) =>
-            {
-                localizationIDField.label = label;
-                localizationIDField.tooltip = tooltip;
-            });
-
-            var displayNameField = visualTree.Q<TextField>("displayName");
-
-            displayNameField.label = _displayName?.S();
-            displayNameField.tooltip = _displayName?.Tooltip();
-
-            _displayName?.Register((label, tooltip) =>
-            {
-                displayNameField.label = label;
-                displayNameField.tooltip = tooltip;
-            });
-
-            var translatesListView = visualTree.Q<ListView>("translates");
-            translatesListView.headerTitle = _translates?.S();
-            translatesListView.tooltip = _translates?.Tooltip();
-
-            _translates?.Register((label, tooltip) =>
-            {
-                translatesListView.headerTitle = label;
-                translatesListView.tooltip = tooltip;
-            });
-
             root.Add(visualTree);
             GlobalLocalization.DefaultHelper.SelectLanguageElement(root);
             return root;
--- Packages/io.github.ykysnk.localization/Editor/BasicTranslateDrawer.cs
+++ Packages/io.github.ykysnk.localization/Editor/BasicTranslateDrawer.cs
@@ -10,18 +10,6 @@ namespace io.github.ykysnk.Localization.Editor
     [CustomPropertyDrawer(typeof(BasicTranslate))]
     public class BasicTranslateDrawer : PropertyDrawer
     {
-        private static readonly string LocalizationName =
-            GlobalLocalization.NameToLocalizationName(nameof(BasicTranslate));
-
-        private static readonly string LocalizationKeyID =
-            $"label.{LocalizationName}.key";
-
-        private static readonly string LocalizationTranslateID = $"label.{LocalizationName}.translate";
-        private static readonly string LocalizationTooltipID = $"label.{LocalizationName}.tooltip";
-        private static readonly string LocalizationCopyID = $"label.{LocalizationName}.copy";
-        private static readonly string LocalizationPasteID = $"label.{LocalizationName}.paste";
-        private static readonly string LocalizationMiscID = $"label.{LocalizationName}.misc";
-
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
             var root = new VisualElement();
@@ -36,51 +24,14 @@ namespace io.github.ykysnk.Localization.Editor
             }
 
             var visualTree = uxml.CloneTree();
+            GlobalLocalization.DefaultHelper.UILocalize(visualTree);
             root.Bind(property.serializedObject);
 
             var keyField = visualTree.Q<TextField>("key");
-            keyField.label = LocalizationKeyID.S();
-            keyField.tooltip = LocalizationKeyID.Tooltip();
-
-            LocalizationKeyID.Register((label, tooltip) =>
-            {
-                keyField.label = label;
-                keyField.tooltip = tooltip;
-            });
-
             var translateField = visualTree.Q<TextField>("translate");
-            translateField.label = LocalizationTranslateID.S();
-            translateField.tooltip = LocalizationTranslateID.Tooltip();
-
-            LocalizationTranslateID.Register((label, tooltip) =>
-            {
-                translateField.label = label;
-                translateField.tooltip = tooltip;
-            });
-
             var tooltipField = visualTree.Q<TextField>("tooltip");
-            tooltipField.label = LocalizationTooltipID.S();
-            tooltipField.tooltip = LocalizationTooltipID.Tooltip();
-
-            LocalizationTooltipID.Register((label, tooltip) =>
-            {
-                tooltipField.label = label;
-                tooltipField.tooltip = tooltip;
-            });
-
-            var miscFoldout = visualTree.Q<Foldout>("misc");
-            miscFoldout.text = LocalizationMiscID.S();
-            miscFoldout.tooltip = LocalizationMiscID.Tooltip();
-
-            LocalizationMiscID.Register((label, tooltip) =>
-            {
-                miscFoldout.text = label;
-                miscFoldout.tooltip = tooltip;
-            });
 
             var copyButton = visualTree.Q<Button>("copy");
-            copyButton.text = LocalizationCopyID.S();
-            copyButton.tooltip = LocalizationCopyID.Tooltip();
             copyButton.clicked += () =>
             {
                 var copyBasicTranslate = new BasicTranslate
@@ -93,15 +44,7 @@ namespace io.github.ykysnk.Localization.Editor
                 EditorGUIUtility.systemCopyBuffer = JsonUtility.ToJson(copyBasicTranslate);
             };
 
-            LocalizationCopyID.Register((label, tooltip) =>
-            {
-                copyButton.text = label;
-                copyButton.tooltip = tooltip;
-            });
-
             var pasteButton = visualTree.Q<Button>("paste");
-            pasteButton.text = LocalizationPasteID.S();
-            pasteButton.tooltip = LocalizationPasteID.Tooltip();
             pasteButton.clicked += () =>
             {
                 try
@@ -116,12 +59,6 @@ namespace io.github.ykysnk.Localization.Editor
                     Utils.LogWarning(nameof(BasicTranslateDrawer), $"Failed to paste: {e.Message}\n{e.StackTrace}");
                 }
             };
-
-            LocalizationPasteID.Register((label, tooltip) =>
-            {
-                pasteButton.text = label;
-                pasteButton.tooltip = tooltip;
-            });
 
             root.Add(visualTree);
             return root;
